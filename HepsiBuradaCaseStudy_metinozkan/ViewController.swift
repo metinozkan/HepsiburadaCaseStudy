b1f07a68076a5ead1c9655fcd938954f7831f7c6
metinozkan <metin.ozkn@hotmail.com>
2021-10-28 14:57:03 +0300

    Initial Commit

---
+++ HepsiBuradaCaseStudy_metinozkan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HepsiBuradaCaseStudy_metinozkan
+//
+//  Created by Metin özkan on 28.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HepsiBuradaCaseStudy_metinozkanUITests/HepsiBuradaCaseStudy_metinozkanUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HepsiBuradaCaseStudy_metinozkanUITestsLaunchTests.swift
+//  HepsiBuradaCaseStudy_metinozkanUITests
+//
+//  Created by Metin özkan on 28.10.2021.
+//
+
+import XCTest
+
+class HepsiBuradaCaseStudy_metinozkanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
