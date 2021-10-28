38995078996d9ea18b29561ae831f7b1722e0d52
metinozkan <metin.ozkn@hotmail.com>
2021-10-28 19:17:22 +0300

    create project folders, MainviewController

---
+++ HepsiBuradaCaseStudy_metinozkan/Controllers/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  HepsiBuradaCaseStudy_metinozkan
+//
+//  Created by Metin özkan on 28.10.2021.
+//
+
+import Foundation
+import UIKit
+class DetailViewController: UIViewController {
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+    
+}
---
+++ HepsiBuradaCaseStudy_metinozkan/Controllers/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  HepsiBuradaCaseStudy_metinozkan
+//
+//  Created by Metin özkan on 28.10.2021.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+    
+}
